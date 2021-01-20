6185b220f6981bb7213fb6d32a4330acf2e6c8c0
Jill Centers <jillcenters@protonmail.com>
2021-01-20 18:25:38 -0500

    Adding Unity Files

---
+++ New Unity Project/Assets/scripts/ballooncontrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballooncontrol : MonoBehaviour
+{
+    public int clicksToPop = 3;
+
+    void OnMouseDown ()
+    {
+        clicksToPop--;
+
+        transform.localScale += new Vector3(0.2f, 0.2f, 0.2f);
+
+        if(clicksToPop == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/scripts/firstScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class firstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int score = 0;
+
+        Debug.Log(score);
+
+        int numA = 20;
+        int numB = 15;
+
+        if(numA > numB)
+        {
+            Debug.Log("numA is greater than numB");
+        } 
+        else if(numA < numB)
+        {
+            Debug.Log("numA is less than numB");
+        }
+        else
+        {
+            Debug.Log("numA is equal to numB");
+        }
+
+        Debug.Log(transform.position);
+
+        Vector3 testVector = new Vector3(10, 2, -5);
+
+        transform.position = testVector;
+
+        Debug.Log(transform.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
